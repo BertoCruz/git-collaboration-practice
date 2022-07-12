3b457e0319670cf0ed8fbd70b2d2332e54aca034
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-07-11 19:45:42 -0600

    Added function feature to practice2.js file

--- practice2.js
+++ practice2.js
@@ -3,10 +3,13 @@ class NewName{
     this.name = name || "Jedeo";
     this.age = age || 27;
     this.state = state || "Colorado";
-
+    this.message = function(message) {
+      return `Hey, what's up, my name is ${this.name} and I am ${this.age} years old. ${message}.`
+    }
   }
 }
 
 var newPerson = new NewName("Dani", 28, "Illinois");
 
 console.log(newPerson);
+console.log(newPerson.message(" I Love Nature, as well"))
